9bb9f7fc1041c3a2d6ac747b6f22f93d4be5c1f7
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-10-31 18:58:45 -0300

    Build Get Todo REST API
    lançando excessão se id não encontrado

--- src/main/java/net/javaguides/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/net/javaguides/todo/service/impl/TodoServiceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.todo.service.impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.todo.dto.TodoDto;
 import net.javaguides.todo.entity.Todo;
+import net.javaguides.todo.exception.ResourceNotFoundException;
 import net.javaguides.todo.repository.TodoRepository;
 import net.javaguides.todo.service.TodoService;
 import org.modelmapper.ModelMapper;
@@ -27,5 +28,15 @@ public class TodoServiceImpl implements TodoService {
         TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
 
         return savedTodoDto;
+    }
+
+    @Override
+    public TodoDto getTodo(Long id) {
+
+        Todo todo = todoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Todo not found with id: "+id));
+
+
+        return modelMapper.map(todo, TodoDto.class);
     }
 }
